7376aa6bc45ceaa930935917bc5068dee5962803
Ali Gangji <ali@neonrain.com>
2016-06-18 11:12:50 -0600

    Initial commit

---
+++ Example/XMPPClient/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  XMPPClient
+//
+//  Created by Ali Gangji on 06/18/2016.
+//  Copyright (c) 2016 Ali Gangji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
